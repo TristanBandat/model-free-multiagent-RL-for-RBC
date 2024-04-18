556fe427c70bed33592c9042fd4af3acbf2893b1
Tristan Bandat <tristan.bandat@gmail.com>
2024-04-18 12:41:41 +0200

    Implement main function
    Main function is handling a game of 2 models against each other
    and returns the results as a printed string.

--- main.py
+++ main.py
@@ -13,7 +13,30 @@ from reconchess import *
 
 
 def main() -> None:
-    pass
+    # TODO: add model name here
+    player_1 = ''
+    player_2 = ''
+    number_of_games_per_color = 2
+    time = 900
+    player_1_wins = 0
+    player_2_wins = 0
+
+    for game_num in range(number_of_games_per_color):
+        winner = play_game(player_1, player_2, time)
+        if winner == 'white':
+            player_1_wins += 1
+        else:
+            player_2_wins += 1
+
+    for game_num in range(number_of_games_per_color):
+        winner = play_game(player_2, player_1, time)
+        if winner == 'white':
+            player_2_wins += 1
+        else:
+            player_1_wins += 1
+
+    print(f'Player 1 {player_1} wins: {str(player_1_wins)}')
+    print(f'Player 2 {player_2} wins: {str(player_2_wins)}')
 
 
 if __name__ == "__main__":
